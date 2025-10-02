bc3c29f2f8851981e5a767296e047b32ee2aad03
The30thHunter <116296560+The30thHunter@users.noreply.github.com>
2025-10-02 20:02:14 -0300

    Update models.py

--- Prototipo_Proyecto_Integrado/Modelos/models.py
+++ Prototipo_Proyecto_Integrado/Modelos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    Nombre = models.CharField(max_length=20)
+    Apellido = models.CharField(max_length=20)
+    Numero_telefono= models.CharField(max_length=15)
+    Direccion = models.CharField(max_length=50)
+    Rut = models.IntegreField()
+
+    def __str__(self):
+        return self.Rut
+    
+class Pedido(models.Model):
+    N_Orden = models.IntegerField()
+    Fecha = models.CharField(max_length=30)
+    Coste = models.IntegerField()
+    Abono = models.IntegerField()
+    Restante = models.IntegerField()
+    def __str__(self):
+        return self.N_Orden
+    
