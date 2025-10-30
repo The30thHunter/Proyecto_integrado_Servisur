dc813272d713837d51562ead62d9bb2c761a8de3
The30thHunter <116296560+The30thHunter@users.noreply.github.com>
2025-10-30 15:07:06 -0300

    creados los modelos de la base de datos

--- Servisur_Django/Base_Datos/models.py
+++ Servisur_Django/Base_Datos/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    Nombre = models.CharField(max_length=20)
+    Apellido = models.CharField(max_length=20)
+    Numero_telefono= models.CharField(max_length=15)
+    Direccion = models.CharField(max_length=50)
+    Rut = models.IntegerField()
+    Activo = models.BooleanField(default=True)
+    def __str__(self):
+        return self.Nombre+" "+self.Apellido
+    
+
+
+    
+class Marca(models.Model):
+    Marca = models.CharField(max_length=20)
+    def __str__(self):
+        return self.Marca
+    
+class Modelo(models.Model):
+    Modelo = models.CharField(max_length=50)
+    Marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Modelo
+
+class Dispositivo(models.Model):
+    Nombre = models.CharField(max_length=30)
+    Trabajo_realizado = models.CharField(max_length=100)
+    Codigo_Bloqueo = models.CharField(max_length=50)
+    modelo = models.ForeignKey(Modelo,on_delete=models.CASCADE)
+    Activo = models.BooleanField(default=True)
+    def __str__(self):
+        return self.Nombre
+    
+class Pedido(models.Model):
+    N_Orden = models.AutoField(primary_key=True)
+    Fecha = models.CharField(max_length=30)
+    Coste = models.IntegerField()
+    Abono = models.IntegerField()
+    Restante = models.IntegerField()
+    Cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    Activo = models.BooleanField(default=True)
+    Dispositivo = models.ForeignKey(Dispositivo,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.N_Orden)
