25a9e80e152aae4d8274370b165fcfda73cf16c7
The30thHunter <116296560+The30thHunter@users.noreply.github.com>
2025-11-27 19:41:57 -0300

    Agregado entorno virtual?

--- Servisur_Django/Servisur_Django/settings.py
+++ Servisur_Django/Servisur_Django/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
+"No me deja subir las cosas si no le coloco algo al codigo que ya estaba"
 from pathlib import Path
 import os
 
